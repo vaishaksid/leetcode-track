1c0055c22ac0217721d294e4048fa4f983a01af4
vaishaksid <95883054+vaishaksid@users.noreply.github.com>
2025-08-17 20:06:35 -0700

    Time: 649 ms (55.56%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,16 +1,10 @@
-with sub_query as (
 SELECT 
-    customer_id,
-    product_name,
-    CASE 
-        WHEN MAX(product_name = 'A') OVER (PARTITION BY customer_id) = 1
-         AND MAX(product_name = 'B') OVER (PARTITION BY customer_id) = 1
-         AND MAX(product_name = 'C') OVER (PARTITION BY customer_id) = 0
-        THEN 'YES'
-        ELSE 'NO'
-    END AS qualifies
-FROM Orders
-)
-select DISTINCT c.customer_id, c.customer_name from sub_query s
-join Customers c on c.customer_id = s.customer_id
-WHERE qualifies = 'YES'
+o.customer_id,
+c.customer_name
+From Orders as o
+LEFT JOIN Customers as c
+on o.customer_id = c.customer_id
+GROUP BY o.customer_id
+HAVING SUM(product_name='A') > 0
+    AND SUM(product_name='B') > 0
+    AND SUM(product_name='C') = 0
